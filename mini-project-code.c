5f38857a56035616682cf5c906d3ef2d37d48866
Siva Nayudu <91612307+Siva-Nayudu@users.noreply.github.com>
2021-12-02 16:06:03 +0530

    Add files via upload

---
+++ mini-project-code.c
@@ -0,0 +1,54 @@
+#include<LiquidCrystal.h>
+#define GREEN 2
+#define YELLOW 3
+#define RED 4
+#define Buzzer 5
+#define Sensor A0
+
+LiquidCrystal lcd(12, 11, 9, 8, 7, 6); //Sets the interfacing pins
+
+void setup()
+{
+	for(int i=2; i<6; i++)
+	{
+		pinMode(i,OUTPUT);
+		Serial.begin(9600); 
+	}
+	lcd.begin(16, 2); //Initializes the 16x2 LCD
+	pinMode(10, INPUT);
+}
+
+void loop()
+{
+	for(int i=2;i<6;i++)
+	{
+		digitalWrite(i,LOW);
+		status_indicator();
+	}
+}
+void status_indicator()
+{
+	lcd.setCursor(0,0); //Sets the position of Output in LCD screen
+	int sensor_val=analogRead(A0);
+	Serial.println(sensor_val);
+	if(sensor_val>=150)
+	{
+		digitalWrite(RED, HIGH);
+		if(digitalRead(10)==1)
+		{
+			digitalWrite(Buzzer, HIGH);
+		}
+		lcd.print("DANGER: HIGH    "); //Prints the output on LCD 
+	}
+	else if(sensor_val>=120)
+	{
+		digitalWrite(YELLOW, HIGH);
+		lcd.print("DANGER: MODERATE"); //Prints the output on LCD 
+	}
+	else
+	{
+	     digitalWrite(GREEN, HIGH);
+		 lcd.print("DANGER: LOW    "); //Prints the output on LCD 
+	}
+	delay(10); //delay
+}		
